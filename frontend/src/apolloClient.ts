d445f8a3e6beaaa7d190231e81d205e7c354bc12
unknown <javierbran73@gmail.com>
2025-08-20 11:51:13 -0600

    Initial commit

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "/api/graphql", // URL de la función Lambda
+  uri: "/.netlify/functions/graphql", // URL de la función Lambda
   cache: new InMemoryCache(),
 });
 
